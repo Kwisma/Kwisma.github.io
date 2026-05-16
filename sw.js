6447710fd79c4cc6756adbb2b7d6ba2be3545e8d
Kwisma <77215384+Kwisma@users.noreply.github.com>
2026-05-16 08:30:18 +0000

    我的第一次

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1778916108434";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1778920212545";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
